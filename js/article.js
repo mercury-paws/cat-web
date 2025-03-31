d043056c406a74a164c46e484a387e747463a340
Anastasiia Kutsenko <145927427+mercury-paws@users.noreply.github.com>
2025-03-31 18:47:50 +0200

    article ts-js

--- js/article.js
+++ js/article.js
@@ -1,32 +1,40 @@
-'use strict'
-
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const container = document.getElementById("container");
 axios.defaults.baseURL = "https://profile-server-qbyd.onrender.com";
 axios.defaults.withCredentials = true;
-
-async function fetchArticle(id) {
-    try {
-      const response = await axios.get(`/cat-article/${id}`);
-      console.log(response.data.data)
-      return response.data.data;
-    } catch (error) {
-      throw new Error(`Error fetching cat-article: ${error.response?.data?.message || error.message}`);
-    }  
-  }
-
-  async function renderArticle() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const catArticleId = urlParams.get("id");
-
-    if (!catArticleId) {
-        console.error("No article ID provided.");
-        return;
-    }
-
-    const article = await fetchArticle(catArticleId);
-    if (article) {
-      document.getElementById("container").innerHTML = DOMPurify.sanitize(article.text);
-    }
+function fetchArticle(id) {
+    return __awaiter(this, void 0, void 0, function* () {
+        var _a, _b;
+        try {
+            const response = yield axios.get(`/cat-article/${id}`);
+            console.log(response.data.data);
+            return response.data.data;
+        }
+        catch (error) {
+            throw new Error(`Error fetching cat-article: ${((_b = (_a = error.response) === null || _a === void 0 ? void 0 : _a.data) === null || _b === void 0 ? void 0 : _b.message) || error.message}`);
+        }
+    });
 }
-
-
+function renderArticle() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const urlParams = new URLSearchParams(window.location.search);
+        const catArticleId = urlParams.get("id");
+        if (!catArticleId) {
+            console.error("No article ID provided.");
+            return;
+        }
+        const article = yield fetchArticle(catArticleId);
+        if (article) {
+            container.innerHTML = DOMPurify.sanitize(article.text);
+        }
+    });
+}
 window.onload = renderArticle;
---
+++ js/article.ts
@@ -0,0 +1,36 @@
+declare const axios: any;
+const container = document.getElementById("container") as HTMLElement;
+declare const DOMPurify: {
+  sanitize: (input: string) => string;
+};
+
+axios.defaults.baseURL = "https://profile-server-qbyd.onrender.com";
+axios.defaults.withCredentials = true;
+
+async function fetchArticle(id: string) {
+    try {
+      const response = await axios.get(`/cat-article/${id}`);
+      console.log(response.data.data)
+      return response.data.data;
+    } catch (error:any) {
+      throw new Error(`Error fetching cat-article: ${error.response?.data?.message || error.message}`);
+    }  
+  }
+
+  async function renderArticle() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const catArticleId = urlParams.get("id");
+
+    if (!catArticleId) {
+        console.error("No article ID provided.");
+        return;
+    }
+
+    const article = await fetchArticle(catArticleId);
+    if (article) {
+      container.innerHTML = DOMPurify.sanitize(article.text);
+    }
+}
+
+
+window.onload = renderArticle;
